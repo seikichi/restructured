62c623042fbc5e253ed7c7d33577faf0c84dde91
Seiichi KONDO <seikichi@kmc.gr.jp>
2016-01-02 00:46:34 +0900

    Change class, module and files names

---
+++ config/filter.js
@@ -0,0 +1,1 @@
+module.exports = function(path) { return /.pegjs$/.test(path); };
--- test/rst.js
+++
@@ -1,8 +0,0 @@
-import assert from 'power-assert';
-import rst from '../lib/rst';
-
-describe('add', () => {
-  it('should parse', () => {
-    assert.deepEqual(rst.parse('hoge'), ['h', 'o', 'g', 'e']);
-  });
-});
