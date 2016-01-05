23adea1d91c9ac808a59c32bef7ff2cadc08dcad
Seiichi KONDO <seikichi@kmc.gr.jp>
2016-01-06 07:36:07 +0900

    Add test case for emphasis inline markup

--- test/parser/inline_markup_emphasis.js
+++ test/parser/inline_markup_emphasis.js
@@ -12,7 +12,37 @@ describe('RST.parse', () => {
     [
       'an emphasis',
       '*emphasis*\n',
-      [new Paragraph({ children: [new Emphasis({ children: [new Text({ text: 'emphasis' })] })] })],
+      [
+        new Paragraph({
+          children: [
+            new Emphasis({ children: [new Text({ text: 'emphasis' })] }),
+            new Text({ text: '\n' }),
+          ],
+        }),
+      ],
+    ],
+    [
+      'multiple emphasis',
+      `\
+l'*emphasis* with the *emphasis*' apostrophe.
+l\u2019*emphasis* with the *emphasis*\u2019 apostrophe.
+`,
+      [
+        new Paragraph({
+          children: [
+            new Text({ text: 'l\'' }),
+            new Emphasis({ children: [new Text({ text: 'emphasis' })] }),
+            new Text({ text: ' with the ' }),
+            new Emphasis({ children: [new Text({ text: 'emphasis' })] }),
+            new Text({ text: '\' apostrophe.\n' }),
+            new Text({ text: 'l\u2019' }),
+            new Emphasis({ children: [new Text({ text: 'emphasis' })] }),
+            new Text({ text: ' with the ' }),
+            new Emphasis({ children: [new Text({ text: 'emphasis' })] }),
+            new Text({ text: '\u2019 apostrophe.\n' }),
+          ],
+        }),
+      ],
     ],
   ].forEach(([title, input, children]) => {
     it(`should parse ${title} correctly`, () => {
