01a2085a1c135425824b940285a3f52a46e737a9
Seiichi KONDO <seikichi@kmc.gr.jp>
2016-01-10 21:59:50 +0900

    Add some enumerate list tests (not complete)

--- src/ParserUtil.js
+++ src/ParserUtil.js
@@ -114,6 +114,14 @@ const ParserUtil = {
   isInlineMarkupSuffix(c) {
     return PunctuationEndStringSuffix.test(c);
   },
+
+  isUpperRomanNumber(s) {
+    return /^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/.test(s);
+  },
+
+  isLowerRomanNumber(s) {
+    return /^m{0,3}(cm|cd|d?c{0,3})(xc|xl|l?x{0,3})(ix|iv|v?i{0,3})$/.test(s);
+  },
 };
 
 export default ParserUtil;
--- test/parser/EnumeratedListTest.js
+++ test/parser/EnumeratedListTest.js
@@ -6,6 +6,37 @@ import { p, ol, li, t } from '../TestUtils';
 describe('RST.parse', () => {
   [
     [
+      'enumerated list',
+      `\
+1. Item one.
+
+2. Item two.
+
+3. Item three.
+`,
+      [
+        ol(li(p(t('Item one.\n'))),
+           li(p(t('Item two.\n'))),
+           li(p(t('Item three.\n')))),
+      ],
+    ],
+    [
+      'enumerated list without blan lines between items',
+      `\
+No blank lines betwen items:
+
+1. Item one.
+2. Item two.
+3. Item three.
+`,
+      [
+        p(t('No blank lines betwen items:\n')),
+        ol(li(p(t('Item one.\n'))),
+           li(p(t('Item two.\n'))),
+           li(p(t('Item three.\n')))),
+      ],
+    ],
+    [
       'enumerated list with auto enumerator "#"',
       `\
 #. Item one.
@@ -16,6 +47,51 @@ describe('RST.parse', () => {
         ol(li(p(t('Item one.\n'))),
            li(p(t('Item two.\n'))),
            li(p(t('Item three.\n')))),
+      ],
+    ],
+    [
+      'enumerated list that starts with non-ordinal-1',
+      `\
+Start with non-ordinal-1:
+
+0. Item zero.
+1. Item one.
+2. Item two.
+3. Item three.
+
+And again:
+
+2. Item two.
+3. Item three.
+`,
+      [
+        p(t('Start with non-ordinal-1:\n')),
+        ol(li(p(t('Item zero.\n'))),
+           li(p(t('Item one.\n'))),
+           li(p(t('Item two.\n'))),
+           li(p(t('Item three.\n')))),
+        p(t('And again:\n')),
+        ol(li(p(t('Item two.\n'))),
+           li(p(t('Item three.\n')))),
+      ],
+    ],
+    [
+      'enumerated list with multiple body elements',
+      `\
+1. Item one: line 1,
+   line 2.
+2. Item two: line 1,
+   line 2.
+3. Item three: paragraph 1, line 1,
+   line 2.
+
+   Paragraph 2.
+`,
+      [
+        ol(li(p(t('Item one: line 1,\n'), t('line 2.\n'))),
+           li(p(t('Item two: line 1,\n'), t('line 2.\n'))),
+           li(p(t('Item three: paragraph 1, line 1,\n'), t('line 2.\n')),
+              p(t('Paragraph 2.\n')))),
       ],
     ],
   ].forEach(([title, input, children]) => {
