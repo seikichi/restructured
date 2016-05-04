91632bc641c3f8ec6db5a6fc2a876cdb90f65cad
Seiichi KONDO <seikichi@kmc.gr.jp>
2016-05-04 18:49:32 +0900

    Update webpack settings in examples/livedemo

--- examples/livedemo/webpack.config.js
+++ examples/livedemo/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
-          presets: ['es2015'],
+          presets: ['es2015', 'react'],
         },
       },
       {
