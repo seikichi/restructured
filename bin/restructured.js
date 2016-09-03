f4a0d5a063eb3b9d2efa4f6d7124f302eaf8ba7c
Seiichi KONDO <seikichi@kmc.gr.jp>
2016-09-03 10:59:26 +0900

    Add CLI program

---
+++ bin/restructured.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var program = require('commander');
+var restructured = require('../lib/RST.js').default;
+
+program.usage('[options] < file.rst').parse(process.argv)
+
+var input = fs.readFileSync('/dev/stdin', 'utf8');
+console.log(JSON.stringify(restructured.parse(input, program)));
