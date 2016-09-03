302304b7b5f015eaa306aaea9906d35aaca964a2
Seiichi KONDO <seikichi@kmc.gr.jp>
2016-09-03 21:38:27 +0900

    Add traverse API

--- src/RST.js
+++ src/RST.js
@@ -1,8 +1,25 @@
+import { List, Map } from 'immutable';
 import parser from './Parser';
 
 const RST = {
   parse(s, options = {}) {
     return parser.parse(s, options);
+  },
+
+  traverse(node, callbacks) {
+    const { enter = _node => _node } = callbacks;
+    enter(node);
+
+    for (const key of node.keys()) {
+      const value = node.get(key);
+
+      if (List.isList(value)) {
+        value.toSeq().forEach(n => RST.traverse(n, callbacks));
+      }
+      if (Map.isMap(value)) {
+        enter(value, callbacks);
+      }
+    }
   },
 };
 
